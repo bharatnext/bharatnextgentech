9aa376913de9c0c6eb517deac824ee467a07b14e
sarvusahitya <sarvusahitya@gmail.com>
2024-10-15 17:03:56 +0530

    update

---
+++ contact.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+
+<!--
+ // WEBSITE: https://themefisher.com
+ // TWITTER: https://twitter.com/themefisher
+ // FACEBOOK: https://www.facebook.com/themefisher
+ // GITHUB: https://github.com/themefisher/
+-->
+
+<html lang="zxx">
+
+<head>
+
+  <!-- ** Basic Page Needs ** -->
+  <meta charset="utf-8">
+  <title>Megakit | HTML5 Agency Template</title>
+
+  <!-- ** Mobile Specific Metas ** -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Agency HTML Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+  <meta name="author" content="Themefisher">
+  <meta name="generator" content="Themefisher Html5 Agency Template v1.0">
+
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+  <!-- Icon Font Css -->
+  <link rel="stylesheet" href="plugins/themify/css/themify-icons.css">
+  <link rel="stylesheet" href="plugins/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="plugins/magnific-popup/magnific-popup.css">
+  <!-- Owl Carousel CSS -->
+  <link rel="stylesheet" href="plugins/slick/slick.css">
+  <link rel="stylesheet" href="plugins/slick/slick-theme.css">
+
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--Favicon-->
+  <link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+</head>
+
+<body>
+
+  <!-- Header Start -->
+  <header class="navigation">
+    <div class="header-top ">
+      <div class="container">
+        <div class="row justify-content-between align-items-center">
+          <div class="col-lg-2 col-md-4">
+            <div class="header-top-socials text-center text-lg-left d-none text-md-left">
+              <a href="https://www.facebook.com/themefisher" aria-label="facebook"><i class="fab fa-facebook-f"></i></a>
+              <a href="https://twitter.com/themefisher" aria-label="twitter"><i class="fab fa-twitter"></i></a>
+              <a href="https://github.com/themefisher/" aria-label="github"><i class="fab fa-github"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-10 col-md-8 text-center text-lg-right text-md-right">
+            <div class="header-top-info mb-2 mb-md-0">
+              <a href="tel:+91 72850 61176">Call Us : <span>+91 72850 61176</span></a>
+              <a href="mailto:info@bharatnextgentech.com"><i class="fas fa-envelope mr-2"></i><span>info@bharatnextgentech.com</span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="navbar">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <nav class="navbar navbar-expand-lg px-0 py-4">
+              <a class="navbar-brand" href="index.php">
+
+                BharatNextGen<span>Tech.</span>
+              </a>
+
+              <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample09"
+                aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="fa fa-bars"></span>
+              </button>
+
+              <div class="collapse navbar-collapse text-center" id="navbarsExample09">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item @@home">
+                    <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item dropdown @@about">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About <i class="fas fa-chevron-down small"></i></a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                      <li><a class="dropdown-item @@company" href="about.php">Our company</a></li>
+                      <li><a class="dropdown-item @@pricing" href="pricing.php">Pricing</a></li>
+
+                      <li class="dropdown dropdown-submenu dropright">
+                        <a class="dropdown-item dropdown-toggle" href="#!" id="dropdown0301" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</a>
+
+                        <ul class="dropdown-menu" aria-labelledby="dropdown0301">
+                          <li><a class="dropdown-item" href="index.php">Submenu 01</a></li>
+                          <li><a class="dropdown-item" href="index.php">Submenu 02</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item @@service"><a class="nav-link" href="service.php">Services</a></li>
+                  <li class="nav-item @@project"><a class="nav-link" href="project.php">Portfolio</a></li>
+                  <li class="nav-item dropdown @@blog">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog <i class="fas fa-chevron-down small"></i></a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown05">
+                      <li><a class="dropdown-item @@blogGrid" href="blog-grid.php">Blog Grid</a></li>
+                      <li><a class="dropdown-item @@blogSidebar" href="blog-sidebar.php">Blog with Sidebar</a></li>
+                      <li><a class="dropdown-item @@blogSingle" href="blog-single.php">Blog Single</a></li>
+
+                      <li class="dropdown dropdown-submenu dropleft">
+                        <a class="dropdown-item dropdown-toggle" href="#!" id="dropdown0501" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</a>
+
+                        <ul class="dropdown-menu" aria-labelledby="dropdown0501">
+                          <li><a class="dropdown-item" href="index.php">Submenu 01</a></li>
+                          <li><a class="dropdown-item" href="index.php">Submenu 02</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item active"><a class="nav-link" href="contact.php">Contact</a></li>
+                </ul>
+
+                <div class="my-2 my-md-0 ml-lg-4 text-center">
+                  <a href="contact.php" class="btn btn-solid-border btn-round-full">Get a Quote</a>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- Header Close -->
+
+  <section class="page-title bg-1">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="block text-center">
+            <span class="text-white">Contact Us</span>
+            <h1 class="text-capitalize mb-4 text-lg">Get in Touch</h1>
+            <ul class="list-inline">
+              <li class="list-inline-item"><a href="index.php" class="text-white">Home</a></li>
+              <li class="list-inline-item"><span class="text-white">/</span></li>
+              <li class="list-inline-item text-white-50">Contact Us</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- contact form start -->
+  <section class="contact-form-wrap section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-12 col-sm-12 d-none">
+          <form id="contact-form" class="contact__form" method="post" action="index.php">
+            <h3 class="text-md mb-4">Contact Form</h3>
+            <div class="form-group">
+              <input name="name" type="text" class="form-control" placeholder="Your Name" required>
+            </div>
+            <div class="form-group">
+              <input name="email" type="email" class="form-control" placeholder="Email Address" required>
+            </div>
+            <div class="form-group-2 mb-4">
+              <textarea name="message" class="form-control" rows="4" placeholder="Your Message" required></textarea>
+            </div>
+            <button class="btn btn-main" name="submit" type="submit">Send Message</button>
+          </form>
+        </div>
+
+        <div class="col-lg-12 col-sm-12">
+          <div class="contact-content pl-lg-5 mt-5 mt-lg-0">
+            <span class="text-muted">We are Professionals</span>
+            <h2 class="mb-5 mt-2">Don’t Hesitate to contact with us for any kind of information</h2>
+
+            <ul class="address-block list-unstyled">
+              <li>
+                <i class="ti-direction mr-3"></i>Sardar Chowk, Opp. Alfa Training Center, Chitaliya Road, Jasdan:- 360050, Rajkot, Gujarat, India
+              </li>
+              <li>
+                <i class="ti-email mr-3"></i>Email: info@bharatnextgenetech.com
+              </li>
+              <li>
+                <i class="ti-mobile mr-3"></i>Phone:+91 72850 61176
+              </li>
+            </ul>
+
+            <ul class="social-icons list-inline mt-5 d-none">
+              <li class="list-inline-item">
+                <a href="http://www.themefisher.com"><i class="fab fa-facebook-f"></i></a>
+              </li>
+              <li class="list-inline-item">
+                <a href="http://www.themefisher.com"><i class="fab fa-twitter"></i></a>
+              </li>
+              <li class="list-inline-item">
+                <a href="http://www.themefisher.com"><i class="fab fa-linkedin-in"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <div class="google-map d-none">
+    <div id="map" data-latitude="40.712776" data-longitude="-74.005974" data-marker="images/marker.png" data-marker-name="Megakit"></div>
+  </div>
+
+  <footer class="footer section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="widget">
+            <h4 class="text-capitalize mb-4">Company</h4>
+
+            <ul class="list-unstyled footer-menu lh-35">
+              <li><a href="about.php">Terms & Conditions</a></li>
+              <li><a href="about.php">Privacy Policy</a></li>
+              <li><a href="contact.php">Support</a></li>
+
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="widget">
+            <h4 class="text-capitalize mb-4">Quick Links</h4>
+
+            <ul class="list-unstyled footer-menu lh-35">
+              <li><a href="about.php">About</a></li>
+              <li><a href="service.php">Services</a></li>
+
+              <li><a href="contact.php">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 mx-auto">
+          <div class="widget">
+            <h4 class="text-capitalize mb-4">Subscribe Us</h4>
+            <p>Subscribe to get latest news article and resources </p>
+
+            <form action="#" class="sub-form">
+              <input type="text" class="form-control mb-3" placeholder="Subscribe Now ...">
+              <a href="#" class="btn btn-main btn-small">subscribe</a>
+            </form>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-sm-6">
+          <div class="widget">
+            <div class="logo mb-4">
+              <h3>
+                BharatNextGen<span>Tech.</span></h3>
+            </div>
+            <h6><a href="mailto:info@bharatnextgentech.com">info@bharatnextgenetech.com</a></h6>
+            <a href="tel:+91 72850 61176"><span class="text-color h4">+91 7285061176</span></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer-btm pt-4">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="copyright">
+              Copyright &copy; 2020, Designed &amp; Developed by BharatNextGenTech.
+            </div>
+          </div>
+          <div class="col-lg-6 text-left text-lg-right d-none">
+            <ul class="list-inline footer-socials">
+              <li class="list-inline-item"><a href="https://www.facebook.com/themefisher"><i class="fab fa-facebook-f mr-2"></i>Facebook</a></li>
+              <li class="list-inline-item"><a href="https://twitter.com/themefisher"><i class="fab fa-twitter mr-2"></i>Twitter</a></li>
+              <li class="list-inline-item"><a href="https://www.pinterest.com/themefisher/"><i class="fab fa-pinterest-p mr-2 "></i>Pinterest</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!--Scroll to top-->
+  <div id="scroll-to-top" class="scroll-to-top">
+    <span class="icon fa fa-angle-up"></span>
+  </div>
+
+
+  <!-- 
+Essential Scripts
+=====================================-->
+  <!-- Main jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4.3.1 -->
+  <script src="plugins/bootstrap/bootstrap.min.js"></script>
+  <!--  Magnific Popup-->
+  <script src="plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+  <!-- Slick Slider -->
+  <script src="plugins/slick/slick.min.js"></script>
+  <!-- Counterup -->
+  <script src="plugins/counterup/jquery.waypoints.min.js"></script>
+  <script src="plugins/counterup/jquery.counterup.min.js"></script>
+
+  <!-- Google Map -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU" defer></script>
+  <script src="plugins/google-map/map.js" defer></script>
+
+  <script src="js/script.js"></script>
+
+</body>
+
+</html>
