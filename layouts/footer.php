fc6ddb884e3b7c364c1dd0525beb91afb64bd54f
kalpesh <kalpeshbavaliya2005@gmail.com>
2024-12-21 22:04:39 +0530

    model in footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -65,3 +65,101 @@
 <div id="scroll-to-top" class="scroll-to-top">
     <span class="icon fa fa-angle-up"></span>
 </div>
+
+<!-- model of contact us  -->
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    Launch static backdrop modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                    <div class="row justify-content-around">
+                        <div class="contact-detail col-12 col-lg-4 pt-4 px-5 ">
+                            <div class="mb-5">
+                                <h5 class="mt-5">Contact Information</h5>
+                            </div>
+
+                            <a href="tel:+91 97247 36947">
+                                <div class="info-call d-flex">
+                                    <p><i class="fa-solid fa-phone"></i></p>
+                                    <p>+91 97247 36947</p>
+                                </div>
+                            </a>
+
+                            <a href="mailto:bharatnextgentech@gmail.com">
+                                <div class="info-call d-flex">
+                                    <p><i class="fa-regular fa-envelope"></i></p>
+                                    <p>bharatnextgentech@gmail.com</p>
+                                </div>
+                            </a>
+                            <div class="info-call d-flex">
+                                <p><i class="fa-solid fa-location-dot"></i></p>
+                                <p>Sardar Chowk, <br> Opp. Alfa Training Center, <br> Chitaliya Road, <br>Jasadan
+                                    360050,
+                                    <br>Rajkot,
+                                    Gujarat,India
+                                </p>
+                            </div>
+                            <a href="https://maps.app.goo.gl/oAkkK5PGrjpPjjpi7" target="_blank">
+                                <div class="info-call my-2 mb-sm-5 mb-lg-5"> <button class=" btn btn-main p-2 ms-4"
+                                        type="button" id="submitBtn1"> <i class="fa-solid fa-location-dot"></i>
+                                        &nbsp;Get
+                                        Direction</button>
+                                </div>
+                            </a>
+                            <div class="contact-shape"></div>
+                        </div>
+                        <div class="contact-form col-12 col-lg-6 shadow-lg p-3 mt-5">
+                            <form class="row g-3 mt-4" id="contactForm">
+                                <div class="col-12">
+                                    <label for="Name"></label>
+                                    <input type="text" class="form-control" id="name" placeholder="Your Name">
+                                </div>
+                                <div class="col-12 mt-1 m-auto">
+                                    <label for="mobile"></label>
+                                    <input type="number" class="form-control" id="mobile" required
+                                        placeholder=" +91 99999 00000">
+                                </div>
+
+                                <div class="col-12">
+                                    <label for="email"></label>
+                                    <input type="email" class="form-control" id="email" placeholder="E-mail (optinal)">
+                                </div>
+                                <div class="col-12 m-auto">
+                                    <label for="city"></label>
+                                    <input type="text" class="form-control" id="city" placeholder="Your City">
+                                </div>
+
+                                <div class="col-12">
+                                    <label for="message"></label>
+                                    <input type="text" cols="25" rows="5" class="form-control" id="message"
+                                        placeholder="Your Message">
+                                </div>
+
+                                <div class="col-12 " id="responseMessage"></div>
+                                <div class="col-12"> <button class="btn btn-main mt-4 col-12" type="button"
+                                        id="submitBtn">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+        </div>
+    </div>
+</div>
